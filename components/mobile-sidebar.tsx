65bee83c4bcfc3d0c324b6b2ae56f264c9a3d567
ubaig517 <ubaig517@gmail.com>
2023-08-16 19:55:54 -0400

    Integrate stripe for payment and billing. Handle application UI for Pro users.

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -10,9 +10,13 @@ import Sidebar from "@/components/sidebar";
 
 interface MobileSidebarProps {
   apiLimitCount: number;
+  isPro: boolean;
 }
 
-const MobileSidebar = ({ apiLimitCount }: MobileSidebarProps) => {
+const MobileSidebar = ({
+  apiLimitCount,
+  isPro = false,
+}: MobileSidebarProps) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -29,7 +33,7 @@ const MobileSidebar = ({ apiLimitCount }: MobileSidebarProps) => {
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
-        <Sidebar apiLimitCount={apiLimitCount} />
+        <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
+import { Check, Zap } from "lucide-react";
+
 import {
   Dialog,
   DialogContent,
@@ -13,11 +17,24 @@ import { Badge } from "@/components/ui/badge";
 import { TOOLS as tools } from "@/constants";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { Check, Zap } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -49,7 +66,13 @@ export const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            disabled={loading}
+            onClick={onSubscribe}
+            size="lg"
+            variant="premium"
+            className="w-full"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
